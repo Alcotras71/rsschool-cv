296aec6afa6512c73a5aab0b212d781fb3776b4f
Andrey <aimdragman2@yandex.ru>
2021-02-27 23:34:23 +0300

    feat: add smooth scroll

--- js/script.js
+++ js/script.js
@@ -4,8 +4,23 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   menuButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
-			menuButtons.forEach(button => button.classList.remove('active'));
+      menuButtons.forEach((button) => button.classList.remove("active"));
       e.target.parentElement.parentElement.classList.add("active");
     });
   });
+
+  const anchors = document.querySelectorAll('a[href*="#"]');
+
+  for (let anchor of anchors) {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const blockID = anchor.getAttribute("href").substr(1);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  }
 });
